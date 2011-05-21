e20347f5a24ca79ba4d9a095be9e6356076b8ca5
pgourlain <pgourlain@hotmail.com>
2011-05-21 22:30:04 +0200

    Adding a plugin to manage custom bookmarks

---
+++ PgoPlugin/AddBookmark.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ICSharpCode.ILSpy;
+
+namespace PgoPlugin
+{
+
+    [ExportContextMenuEntry(Header = "_Add Bookmark", Icon="addbookmark.png")]
+	public class AddBookmark : IContextMenuEntry
+    {
+        #region IContextMenuEntry Members
+
+        public bool IsVisible(ICSharpCode.TreeView.SharpTreeNode[] selectedNodes)
+        {
+            return true;
+        }
+
+        public bool IsEnabled(ICSharpCode.TreeView.SharpTreeNode[] selectedNodes)
+        {
+            return true;
+        }
+
+        public void Execute(ICSharpCode.TreeView.SharpTreeNode[] selectedNodes)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
