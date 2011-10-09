f06743fc611cc5629aad4cfacb2e75f55c19b87c
pgourlain <pgourlain@hotmail.com>
2011-10-09 15:25:51 +0200

    setting assemblies in x86 mode to enabled Debugger on 64 bits machines

--- PgoPlugin/Debugger/CallStackCommand.cs
+++ PgoPlugin/Debugger/CallStackCommand.cs
@@ -7,18 +7,18 @@ using ICSharpCode.ILSpy;
 namespace PgoPlugin.Debugger
 {
 
-    [ExportMainMenuCommand(Menu = "_Debugger", MenuIcon = "callstack.png", Header = "Call stack", MenuCategory = "Open", MenuOrder = 1.5)]
-    class CallStackCommand : SimpleCommand
-    {
-        public override void Execute(object parameter)
-        {
-            SingletonPane<PgoCallStackPane>.Instance.Show();
-        }
+    //[ExportMainMenuCommand(Menu = "_Debugger", MenuIcon = "callstack.png", Header = "Call stack", MenuCategory = "Open", MenuOrder = 1.5)]
+    //class CallStackCommand : SimpleCommand
+    //{
+    //    public override void Execute(object parameter)
+    //    {
+    //        SingletonPane<PgoCallStackPane>.Instance.Show();
+    //    }
 
-        public override bool CanExecute(object parameter)
-        {
-            //return ICSharpCode.ILSpy.Debugger.Services.DebuggerService.CurrentDebugger.IsDebugging;
-            return false;
-        }
-    }
+    //    public override bool CanExecute(object parameter)
+    //    {
+    //        //return ICSharpCode.ILSpy.Debugger.Services.DebuggerService.CurrentDebugger.IsDebugging;
+    //        return false;
+    //    }
+    //}
 }
